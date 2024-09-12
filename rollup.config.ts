2ce9eef34cefe3a3efd45b55dc00b5c9c215cfff
chenhuaiyuan <chenhuaiyuan@algorix.co>
2024-09-12 17:37:38 +0800

    chore: 确定产物地址

--- rollup.config.ts
+++ rollup.config.ts
@@ -1,52 +1,34 @@
-import typescript from "@rollup/plugin-typescript";
-import babel from "@rollup/plugin-babel";
-import json from "@rollup/plugin-json";
 import commonjs from "@rollup/plugin-commonjs";
 import resolve from "@rollup/plugin-node-resolve";
 import dynamicImportVars from "@rollup/plugin-dynamic-import-vars";
-import dts from "rollup-plugin-dts";
+import typescript from "@rollup/plugin-typescript";
+import babel from "@rollup/plugin-babel";
+// import terser from "@rollup/plugin-terser";
 import { defineConfig } from "rollup";
 
 export default defineConfig([
   {
     input: "src/index.ts",
     output: {
-      dir: "dist",
-      format: "esm",
+      dir: "bin",
+      format: "cjs",
+      banner: "#!/usr/bin/env node",
     },
-    // output: [
-    //   {
-    //     dir: "dist/esm",
-    //     format: "esm",
-    //   },
-    //   {
-    //     dir: "dist/cjs",
-    //     format: "cjs",
-    //   },
-    // ],
     plugins: [
-      json(),
       resolve(),
       commonjs({
-        ignoreDynamicRequires: false,
+        // ignoreDynamicRequires: false,
+        include: /node_modules/,
+        sourceMap: false,
       }),
       dynamicImportVars(),
-      typescript({
-        tsconfig: "tsconfig.json",
-      }),
+      typescript(),
       babel({
         babelHelpers: "bundled",
-        presets: ["@babel/preset-env"],
         exclude: "node_modules/**",
       }),
+      // terser(),
     ],
-    external: ["@inquirer/prompts", "find-config"],
-  },
-  {
-    input: "dist/types/src/types.d.ts", // 类型声明入口文件
-    output: {
-      file: "dist/types/index.d.ts", // 输出的类型声明文件
-    },
-    plugins: [dts()],
+    external: ["@inquirer/prompts", "find-config", "fs", "path"],
   },
 ]);
